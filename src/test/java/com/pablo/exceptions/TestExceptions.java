1e1c9020f0345fe26eb2c657eaaa41c03d358b13
pabloalonso-20 <alonso.pablo20@gmail.com>
2020-10-15 15:29:03 -0500

    Added before each and set up on TestExceptions.java

--- src/test/java/com/pablo/exceptions/TestExceptions.java
+++ src/test/java/com/pablo/exceptions/TestExceptions.java
@@ -2,13 +2,23 @@ package com.pablo.exceptions;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Exceptions")
 public class TestExceptions {
+	Main except;
+	
+	@BeforeEach
+	void init() {
+		except = new Main();
+	}
+	
 	
 	@Test
 	public void test() {
-		fail("This is a failing Test");
+		
 	}
 
 }
